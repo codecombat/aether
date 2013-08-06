19555fa12d7ad508049bec01f3e6816463df3fd7
Michael Schmatz <schmatz@umich.edu>
2013-08-05 18:19:14 -0700

    Fixed travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,5 +48,6 @@ module.exports = function(grunt) {
 
   // Default task(s).
   grunt.registerTask('default', ['coffeelint','coffee','jasmine','uglify']);
+  grunt.registerTask('travis',['coffeelint','coffee','jasmine']);
 
 };
