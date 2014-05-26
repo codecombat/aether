af791c269cf43eb3f32735e9d4b204f2732d7c88
Nick Winter <livelily@gmail.com>
2014-05-26 09:04:35 -0700

    Updates

--- dev/index.js
+++ dev/index.js
@@ -42,7 +42,7 @@
   lastProblems = null;
 
   showProblems = function(aether) {
-    var allProblems, ann, annotations, editor, el, level, problem, problemIndex, problems, session, text, treema, treemaOptions, worst, wrapper, _ref;
+    var allProblems, ann, annotations, editor, el, err, level, problem, problemIndex, problems, session, text, treema, treemaOptions, worst, wrapper, _ref;
     if (_.isEqual(aether.problems, lastProblems)) {
       return;
     }
@@ -79,10 +79,15 @@
         }
       }
     };
-    treema = TreemaNode.make(el, treemaOptions);
-    treema.build();
-    $("#aether-problems").empty().append(el);
-    treema.open(2);
+    try {
+      treema = TreemaNode.make(el, treemaOptions);
+      treema.build();
+      $("#aether-problems").empty().append(el);
+      treema.open(2);
+    } catch (_error) {
+      err = _error;
+      console.error("Couldn't make problems Treema:", err);
+    }
     editor = editors[0];
     session = editor.getSession();
     annotations = [];
@@ -115,7 +120,7 @@
   };
 
   showFlow = function(aether) {
-    var el, end, key, markerRange, seen, session, start, state, states, text, treema, treemaOptions, _i, _j, _len, _len1, _ref, _ref1, _ref2, _ref3;
+    var el, end, err, key, markerRange, seen, session, start, state, states, text, treema, treemaOptions, _i, _j, _len, _len1, _ref, _ref1, _ref2, _ref3;
     clearMarkers();
     $("#aether-flow").empty();
     if (!((_ref = aether.flow) != null ? _ref.states : void 0)) {
@@ -160,14 +165,19 @@
         }
       }
     };
-    treema = TreemaNode.make(el, treemaOptions);
-    treema.build();
-    $("#aether-flow").append(el);
-    return treema.open(3);
+    try {
+      treema = TreemaNode.make(el, treemaOptions);
+      treema.build();
+      $("#aether-flow").append(el);
+      return treema.open(3);
+    } catch (_error) {
+      err = _error;
+      return console.error("Couldn't make flow Treema:", err);
+    }
   };
 
   showMetrics = function(aether) {
-    var el, treema, treemaOptions;
+    var el, err, treema, treemaOptions;
     $("#aether-metrics").empty();
     if (!aether.metrics) {
       return;
@@ -180,10 +190,15 @@
         type: "object"
       }
     };
-    treema = TreemaNode.make(el, treemaOptions);
-    treema.build();
-    $("#aether-metrics").append(el);
-    return treema.open(1);
+    try {
+      treema = TreemaNode.make(el, treemaOptions);
+      treema.build();
+      $("#aether-metrics").append(el);
+      return treema.open(1);
+    } catch (_error) {
+      err = _error;
+      return console.error("Couldn't make metrics Treema:", err);
+    }
   };
 
   demoShowOutput = function(aether) {
@@ -201,7 +216,9 @@
     return $("#aether-problems").empty();
   };
 
-  lastJSInputAether = new Aether();
+  lastJSInputAether = new Aether({
+    language: 'python'
+  });
 
   lastAetherInput = "";
 
@@ -209,7 +226,7 @@
     var aetherInput, code;
     aetherInput = editors[1].getValue();
     code = grabDemoCode();
-    if (!Aether.hasChangedSignificantly(code, lastJSInputAether.raw) && aetherInput === lastAetherInput) {
+    if (!lastJSInputAether.hasChangedSignificantly(code, lastJSInputAether.raw) && aetherInput === lastAetherInput) {
       return;
     }
     clearOutput();
@@ -259,27 +276,27 @@
     {
       name: "Basic",
       code: 'function fib(n) {\n  return n < 2 ? n : fib(n - 1) + fib(n - 2);\n}\nvar chupacabra = fib(Math.ceil(Math.random() * 5))\nthis.say("I want", chupacabra, "gold.");\nreturn chupacabra;',
-      aether: 'var aetherOptions = {\n  thisValue: {say: console.log},\n  problems: {jshint_W040: {level: "ignore"}}\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\naether.run();\naether.run();\naether.run();\ndemoShowOutput(aether);'
+      aether: 'var thisValue = {say: console.log};\nvar aetherOptions = {\n  executionLimit: 1000,\n  problems: {jshint_W040: {level: "ignore"}}\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod(thisValue);\naether.run(method);\naether.run(method);\naether.run(method);\ndemoShowOutput(aether);'
     }, {
       name: "Buggy",
       code: 'function distance_squared(from, target) {  // no camelCase\n  if(from.pos)\n    return distance_squared(from.pos, target);  // |from| as Thang\n    if(target.pos)  // weird indentation\n      return distance_squared(from, target.pos);  // |target| as Thang\n  var dx = target.x - from.x; var dy = target.y - from.y;  // bad style: two statements, one line\nreturn dx * dx + dy dy;  // syntax error\n}\n\nvar enemies = getEnemys();  // missing this, also typo in method\nvar nearestEnemy = null  // missing semicolon\nnearestDistance = 9001;  // missing var\nfor(var enemy in enemies) {  // style: somehow warn user that JS loops do not really work like this on arrays?\n  //for(var i = 0; i < enemies.length; ++i) {  // (better method of looping an array)\n  var enemy = enemies[i];\n  var distance = distance_squared(this, enemy);\n  if(distance < nearestDistance) {\n    nearestDistance = distance;\n    nearestEnemy = enemyy;  // typo in local variable\n  }\n//}  // missing curly brace\nthis.markForDeath(nearestEnemy);  // no markForDeath method available in this challenge\nthis.say(nearestEnemy.id, you are going down!);  // missing string quotes, also will have runtime error when nearestEnemy is still null\nnearestEnemy.health = -9001;  // disallowed\nwindow.alert("pwn!");  // nope\ntry {\n  this.die();  // die() is not available, but they are going to handle it, so perhaps it should not be an error?\n}\ncatch (error) {\n  this.say("Could not shuffle mortal coil.");\n}\nthis.  // clearly not done typing\nthis.explode;  // does not do anything because the function is not called\n96;  // no effect\nreturn nearestEnemy;',
-      aether: 'var aetherOptions = {\n  thisValue: {\n    getEnemies: function() { return [{id: "Brack", health: 10, pos: {x: 15, y: 20}}, {id: "Goreball", health: 20, pos: {x: 25, y: 30}}]; },\n    say: console.log,\n    explode: function() { this.say("Exploooode!"); }\n  },\n  problems: {\n    jshint_W040: {level: "ignore"},\n    aether_MissingThis: {level: "warning"}\n  },\n  functionName: "getNearestEnemy",\n  requiresThis: false\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\naether.run();\ndemoShowOutput(aether);'
+      aether: 'var thisValue ={\n  getEnemies: function() { return [{id: "Brack", health: 10, pos: {x: 15, y: 20}}, {id: "Goreball", health: 20, pos: {x: 25, y: 30}}]; },\n  say: console.log,\n  explode: function() { this.say("Exploooode!"); }\n};\nvar aetherOptions = {\n  executionLimit: 1000,\n  problems: {\n    jshint_W040: {level: "ignore"},\n    aether_MissingThis: {level: "warning"}\n  },\n  functionName: "getNearestEnemy",\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod(thisValue);\naether.run(method);\ndemoShowOutput(aether);'
     }, {
       name: "Yield Conditionally",
       code: '// Try your own code here\nvar x = this.charge();\nthis.hesitate();\nthis.hesitate();\nif(retries)\n  return this.planStrategy(retries - 1);\nelse\n  return this.charge();',
-      aether: 'var aetherOptions = {\n  thisValue: {\n    charge: function() { this.say("attack!"); return "attack!"; },\n    hesitate: function() { this.say("uhh..."); this._aetherShouldYield = true; },\n    say: console.log\n  },\n  problems: {\n    jshint_W040: {level: "ignore"},\n    aether_MissingThis: {level: "warning"}\n  },\n  functionName: "planStrategy",\n  functionParameters: ["retries"],\n  yieldConditionally: true,\n  requiresThis: false\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod();\nvar generator = method();\naether.sandboxGenerator(generator);\nvar executeSomeMore = function executeSomeMore() {\n  var result = generator.next();\n  demoShowOutput(aether);\n  if(!result.done)\n    setTimeout(executeSomeMore, 2000);\n};\nexecuteSomeMore();'
+      aether: 'var aetherOptions = {\n  executionLimit: 1000,\n  problems: {\n    jshint_W040: {level: "ignore"},\n    aether_MissingThis: {level: "warning"}\n  },\n  functionName: "planStrategy",\n  functionParameters: ["retries"],\n  yieldConditionally: true,\n};\nvar aether = new Aether(aetherOptions);\nvar thisValue = {\n  charge: function() { this.say("attack!"); return "attack!"; },\n  hesitate: function() { this.say("uhh..."); aether._shouldYield = true; },\n  say: console.log\n};\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod(thisValue);\nvar generator = method();\naether.sandboxGenerator(generator);\nvar executeSomeMore = function executeSomeMore() {\n  var result = generator.next();\n  demoShowOutput(aether);\n  if(!result.done)\n    setTimeout(executeSomeMore, 2000);\n};\nexecuteSomeMore();'
     }, {
       name: "Yield Automatically",
       code: 'for (var i = 1; i <= 100; ++i) {\n  this.print([!(i % 3) ? \'fizz\' : void 0] + [!(i % 5) ? \'buzz\' : void 0] || i);\n}',
-      aether: 'var aetherOptions = {\n  thisValue: {\n    print: console.log\n  },\n  problems: {\n    jshint_W040: {level: "ignore"}\n  },\n  yieldAutomatically: true\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod();\nvar generator = method();\naether.sandboxGenerator(generator);\nvar executeSomeMore = function executeSomeMore() {\n  var result = generator.next();\n  demoShowOutput(aether);\n  if(!result.done)\n    setTimeout(executeSomeMore, 25);\n};\nexecuteSomeMore();'
+      aether: 'var thisValue = {\n  print: console.log\n};\nvar aetherOptions = {\n  executionLimit: 1000,\n  problems: {\n    jshint_W040: {level: "ignore"}\n  },\n  yieldAutomatically: true\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod(thisValue);\nvar generator = method();\naether.sandboxGenerator(generator);\nvar executeSomeMore = function executeSomeMore() {\n  var result = generator.next();\n  demoShowOutput(aether);\n  if(!result.done)\n    setTimeout(executeSomeMore, 25);\n};\nexecuteSomeMore();'
     }, {
       name: "Hacker",
       code: 'var s = \'var x=new XMLHttpRequest();x.open("GET","/auth/whoami",false);x.send();var u=JSON.parse(x.responseText);u.name="Nick!";x=new XMLHttpRequest();x.open("PUT","/db/user/"+u._id,false);x.setRequestHeader("Content-Type","application/json");x.send(JSON.stringify(u));\'\nthis.say("Trying to run hack!");\n(function(){}).__proto__.constructor(s)();\nthis.say("Did we error out?");',
-      aether: 'var aetherOptions = {\n  thisValue: {say: console.log},\n  problems: {jshint_W040: {level: "ignore"}}\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\naether.run();\ndemoShowOutput(aether);'
+      aether: 'var thisValue = {say: console.log};\nvar aetherOptions = {\n  executionLimit: 1000,\n  problems: {jshint_W040: {level: "ignore"}}\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod(thisValue);\naether.run(method);\ndemoShowOutput(aether);'
     }, {
       name: "Rectangles",
       code: 'function largestRectangle(grid, bottomY, leftX, width, height) {\n  var coveredRows = [];\n  var shortestCoveredRow = width - leftX;\n  var done = false;\n  for(var y = bottomY; !done && y < height; ++y) {\n    var coveredRow = 0, done2 = false;\n    for(var x = leftX; !done2 && x < leftX + shortestCoveredRow; ++x) {\n      if(!grid[y][x])\n        ++coveredRow;\n      else\n        done2 = true;\n    }\n    if(!coveredRow)\n      done = true;\n    else {\n      coveredRows.push(coveredRow);\n      shortestCoveredRow = Math.min(shortestCoveredRow, coveredRow);\n    }\n  }\n  var maxArea = 0, maxAreaRows = 0, maxAreaRowLength = 0, shortestRow = 0;\n  for(var rowIndex = 0; rowIndex < coveredRows.length; ++rowIndex) {\n    var rowLength = coveredRows[rowIndex];\n    if(!shortestRow)\n      shortestRow = rowLength;\n    area = rowLength * (rowIndex + 1);\n    if(area > maxArea) {\n      maxAreaRows = rowIndex +1;\n      maxAreaRowLength = shortestRow;\n      maxArea = area;\n    }\n    shortestRow = Math.min(rowLength, shortestRow);\n  }\n  return {x: leftX + maxAreaRowLength / 2, y: bottomY + maxAreaRows / 2, width: maxAreaRowLength, height: maxAreaRows};\n}\n\nvar grid = this.getNavGrid().grid;\nvar tileSize = 4;\nfor(var y = 0; y < grid.length - tileSize / 2; y += tileSize) {\n  for(var x = 0; x < grid[0].length - tileSize / 2; x += tileSize) {\n    var occupied = grid[y][x];\n    if(!occupied) {\n      var rect = largestRectangle(grid, y, x, grid[0].length, grid.length);\n      this.addRect(rect.x, rect.y, rect.width, rect.height);\n      this.say("Placed rect " + rect.x + ", " + rect.y + ", " + rect.width + ", " + rect.height + " for " + grid[0].length + ",  " + grid.length + ", " + x + ", " + y);\n      this.wait(0.1);\n      for(var y2 = rect.y - rect.height / 2; y2 < rect.y + rect.height / 2; ++y2) {\n        for(var x2 = rect.x - rect.width / 2; x2 < rect.x + rect.width / 2; ++x2) {\n          grid[y2][x2] = 1;\n        }\n      }\n    }\n  }\n}',
-      aether: 'var _canvas = [\n  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],\n  [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0],\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],\n  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0],\n  [0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0],\n  [0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0],\n  [0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0],\n  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],\n  [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0],\n  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],\n  [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],\n  [1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0],\n  [1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],\n  [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]\n];\n\nvar aetherOptions = {\n  thisValue: {\n    getNavGrid: function() { return {grid: _canvas }; },\n    addRect: function() { },\n    say: console.log,\n    wait: function() { }\n  },\n  problems: {\n    jshint_W040: {level: "ignore"},\n    aether_MissingThis: {level: "warning"}\n  },\n  functionName: "plan",\n  functionParameters: [],\n  yieldConditionally: true,\n  requiresThis: true\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod();\nvar generator = method();\nif(aetherOptions.yieldConditionally) {\n  var executeSomeMore = function executeSomeMore() {\n    var result = generator.next();\n    demoShowOutput(aether);\n    if(!result.done)\n      setTimeout(executeSomeMore, 2000);\n  };\n  executeSomeMore();\n}\nelse {\n  demoShowOutput(aether);\n}'
+      aether: 'var _canvas = [\n  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],\n  [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0],\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],\n  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],\n  [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0],\n  [0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0],\n  [0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0],\n  [0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0],\n  [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],\n  [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0],\n  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0],\n  [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],\n  [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],\n  [1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0],\n  [1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],\n  [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]\n];\n\nvar thisValue = {\n  getNavGrid: function() { return {grid: _canvas }; },\n  addRect: function() { },\n  say: console.log,\n  wait: function() { }\n};\nvar aetherOptions = {\n  executionLimit: 10000,\n  problems: {\n    jshint_W040: {level: "ignore"},\n    aether_MissingThis: {level: "warning"}\n  },\n  functionName: "plan",\n  functionParameters: [],\n  yieldConditionally: true,\n};\nvar aether = new Aether(aetherOptions);\nvar code = grabDemoCode();\naether.transpile(code);\nvar method = aether.createMethod(thisValue);\nvar generator = method();\nif(aetherOptions.yieldConditionally) {\n  var executeSomeMore = function executeSomeMore() {\n    var result = generator.next();\n    demoShowOutput(aether);\n    if(!result.done)\n      setTimeout(executeSomeMore, 2000);\n  };\n  executeSomeMore();\n}\nelse {\n  demoShowOutput(aether);\n}'
     }
   ];
 
